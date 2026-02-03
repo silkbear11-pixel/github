43e12bb70b4d3f7488910318a8629b55b9a349fb
silkbear11-pixel <silkbear11@gmail.com>
2026-02-02 21:38:09 -0800

    DES 157A Portal

--- MadLibs/v1/script.js
+++ MadLibs/v1/script.js
@@ -1,1 +1,12 @@
 "use strict";
+
+const myForm = document.querySelector("form");
+const inputPage = document.querySelector(".frame");
+const outputPage = document.querySelector(".output");
+
+myForm.addEventListener("submit", function (event) {
+  event.preventDefault();
+
+  inputPage.classList.add("hidden");
+  outputPage.classList.remove("hidden");
+});
