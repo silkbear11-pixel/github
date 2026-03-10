c588878fd5b2528866203e03f6e5980ac1c40fd0
Hyunwoo Song <songhw@scc2102-20.ucdavis.edu>
2026-03-10 16:58:42 -0700

    DES 157A Portal

--- game/v2-final-version/script.js
+++ game/v2-final-version/script.js
@@ -12,9 +12,9 @@
 
   //reduce the html to 1. Make a section for each page. so when start is pressed, the homepage becomes none. Try to keep the volume mixer floated or outside the page, so it doesnt get effected by script.
 
-  //The pop-up screen can be inside the section(game screen) for none again. Or put it as another section and work on that. 
+  //The pop-up screen can be inside the section(game screen) for none again. Or put it as another section and work on that.
 
-  //probability -> javascript. HTML is content, so having 9 dice span is not correct. 
+  //probability -> javascript. HTML is content, so having 9 dice span is not correct.
 
 
   const startBtn = document.querySelector('#start');
@@ -34,7 +34,7 @@
   const rollBtn = document.querySelector('#roll');
   const passBtn = document.querySelector('#pass');
 
-  // if (game && scoreBox && status && rollBtn && passBtn && die1El && die2El) {
+  if (game && scoreBox && status && rollBtn && passBtn && die1El && die2El) {
 
     const gameData = {
       players: ['player 1', 'player 2'],
@@ -43,7 +43,7 @@
       roll2: 0,
       rollSum: 0,
       index: Math.round(Math.random()),
-      gameEnd: 29
+      gameEnd: 30
     };
 
     let gameOver = false;
@@ -64,14 +64,81 @@
     });
 
     function throwDice() {
-      gameData.roll1 = Math.floor(Math.random() * 6) + 1;
-      gameData.roll2 = Math.floor(Math.random() * 6) + 1;
-      gameData.rollSum = gameData.roll1 + gameData.roll2;
+      // set random iteration number
+     
+      let iteration = Math.floor(Math.random() * 6) + 1; //keep it at *6 and +1 for a random number from 1 to 6. But it should be random, is it ok to have a fixed range the same as dice?
+      // need counter start at 0
+      let counter = 0;
+      let die1;
+      let die2;
+      // setInterval to run until counter > iteration at maybe 250ms
+      let diceInterval = setInterval(function () {
+      // if counter <= iteration {
+      if (counter <= iteration) {
+         // show random die number (code below)
+          die1 = Math.floor(Math.random() * 6) + 1;
+          die2 = Math.floor(Math.random() * 6) + 1;
 
-      die1El.setAttribute('data-face', gameData.roll1);
-      die2El.setAttribute('data-face', gameData.roll2);
+          // wrong code noted out because actionArea is not defined and gameData.dice does not exist
+          // actionArea.innerHTML = `<img src="images/${gameData.dice[die1 - 1]}" alt="die ${die1}">  
+          //                         <img src="images/${gameData.dice[die2 - 1]}" alt="die ${die2}">`;
 
-      if (gameData.rollSum === 2) {
+          die1El.setAttribute('data-face', die1);
+          die2El.setAttribute('data-face', die2);
+                                 
+         // increment counter
+         counter++; //++ because my dice count is going up not down
+         // else {
+         } else {
+         // clearInterval when counter > iteration
+         clearInterval(diceInterval);
+
+         gameData.roll1 = die1;
+         gameData.roll2 = die2;
+         gameData.rollSum = gameData.roll1 + gameData.roll2;
+
+         if (gameData.rollSum === 2) {
+           status.textContent = 'Snake Eye! Score reset to 0.';
+           gameData.score[gameData.index] = 0;
+           showCurrentScore();
+           switchPlayer();
+           return;
+         }
+         
+         else if (gameData.roll1 === 1 || gameData.roll2 === 1) {
+           status.textContent = 'Rolled a 1. Turn ends.';
+           switchPlayer();
+           return;
+         }
+         
+         else {
+           gameData.score[gameData.index] = gameData.score[gameData.index] + gameData.rollSum;
+           showCurrentScore();
+           status.textContent = `${gameData.players[gameData.index]} rolled ${gameData.rollSum}. Roll or Pass.`;
+         }
+
+         if (gameData.score[gameData.index] > gameData.gameEnd) {
+           status.textContent = `${gameData.players[gameData.index]} wins! Click Reset to play again.`;
+           gameOver = true;
+         }
+       
+         }
+        }, 250); //250ms, this input changes the rolls done for dice animation
+
+
+      
+     // gameData.roll1 = Math.floor(Math.random() * 6) + 1;
+      //gameData.roll2 = Math.floor(Math.random() * 6) + 1;
+     // gameData.rollSum = gameData.roll1 + gameData.roll2;
+
+      //die1El.setAttribute('data-face', gameData.roll1);
+      //die2El.setAttribute('data-face', gameData.roll2);
+
+      
+
+
+      // change it to if else if for code below
+      /*if (gameData.rollSum === 2) {
         status.textContent = 'Snake Eye! Score reset to 0.';
         gameData.score[gameData.index] = 0;
         showCurrentScore();
@@ -87,13 +154,17 @@
 
       gameData.score[gameData.index] = gameData.score[gameData.index] + gameData.rollSum;
       showCurrentScore();
-      status.textContent = `${gameData.players[gameData.index]} rolled ${gameData.rollSum}. Roll or Pass.`;
+      status.textContent = `${gameData.players[gameData.index]} rolled ${gameData.rollSum}. Roll or Pass.`; */
 
-      if (gameData.score[gameData.index] > gameData.gameEnd) {
+       //checkRolledDice(die1, die2); this code should be placed here before the result is given. Figure out where it should be placed by testing. 
+
+     /* if (gameData.score[gameData.index] > gameData.gameEnd) {
         status.textContent = `${gameData.players[gameData.index]} wins! Click Reset to play again.`;
         gameOver = true;
       }
-    }
+    } */
+
+   }
 
     function switchPlayer() {
       if (gameData.index === 0) {
@@ -110,6 +181,6 @@
                             <p>P2: ${gameData.score[1]}</p>`;
     }
 
-  // }
+  }
 
 })();
